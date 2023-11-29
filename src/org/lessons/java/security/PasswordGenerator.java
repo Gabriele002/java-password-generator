bec73c74f0539d1c7ea95c4a0db1439a25f171f9
Gabriele002 <gabrieleciarleglio97@gmail.com>
2023-11-29 14:20:29 +0100

    Finish work

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -6,7 +6,28 @@ public class PasswordGenerator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        System.out.println("Inserisci il tuo Nome");
+        String userName = scan.nextLine();
 
+        System.out.println("Inserisci il tuo Cognome");
+        String userSurname = scan.nextLine();
+
+        System.out.println("Inserisci il tuo colore preferito");
+        String userPreferiteColor = scan.nextLine();
+
+        System.out.println("Inserisci il tuo giorno di nascita");
+        int userDayOfBirth = scan.nextInt();
+
+        System.out.println("Inserisci il tuo mese di nascita");
+        int userMounthOfBirth = scan.nextInt();
+
+        System.out.println("Inserisci il tuo anno di nascita");
+        int userYearOfBirth = scan.nextInt();
+
+        int sommaMounthDayYearOfBirth = userYearOfBirth + userMounthOfBirth + userDayOfBirth;
+        String pswGenerator = userName + "-"+ userSurname+"-"+userPreferiteColor+"-"+ sommaMounthDayYearOfBirth;
+        System.out.println("La tua nuova psw è:");
+        System.out.println(pswGenerator);
 
         scan.close();
 
